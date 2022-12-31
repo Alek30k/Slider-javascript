a3c9563764231fc3392a4d06a188a064f53d9cd0
Alek30k <alemank23k@gmail.com>
2022-12-30 22:36:05 -0300

    last commit

--- slider.js
+++ slider.js
@@ -1,5 +1,21 @@
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const slider = document.querySelector(".slider");
+const images = document.querySelectorAll(".image");
 
-right.addEventListener("");
+let slideNumber = 1;
+const length = images.length;
+
+const nextSlide = () => {
+  slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+  slideNumber++;
+};
+
+const prevSlide = () => {
+  slider.style.transform = `translateX(0px)`;
+  slideNumber = 1;
+};
+
+right.addEventListener("click", () => {
+  slideNumber < length ? nextSlide() : prevSlide();
+});
